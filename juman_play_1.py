6f46cab83e0e95324c8772070e8f72b0be202462
Hector Garcia <hectorgarcia@gmail.com>
2017-01-13 14:30:34 +0900

    Showing detailed information about sushi that is available in juman++

--- juman_play_1.py
+++ juman_play_1.py
@@ -1,7 +1,7 @@
 from pyknp import Jumanpp
 
 jumanapp = Jumanpp()
-j = jumanapp.analysis("ケーキを食べる")
+j = jumanapp.analysis("寿司とケーキを食べたい")
 
 print("\n------------------------")
 print("Print list of objects:")
@@ -13,3 +13,13 @@ print("Print morpheme midashi from each object in the list:")
 print("------------------------------------------------------")
 print([morpheme.midasi for morpheme in j.mrph_list()])
 
+
+print("\n------------------------------------------------------")
+print("Print sushi detailed information:")
+print("------------------------------------------------------")
+morphemes = [morpheme for morpheme in j.mrph_list()]
+sushi = morphemes[0]
+print(sushi.imis)
+print(sushi.genkei)
+print(sushi.bunrui)
+print(sushi.yomi)
