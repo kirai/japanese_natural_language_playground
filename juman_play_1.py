bf94ce173b57d2cafbe55103cc0e1faa8d6f9f2e
Hector Garcia <hectorgarcia@gmail.com>
2017-01-13 10:20:02 +0900

    Print midashi for each morpheme in the analyzed sentence

--- juman_play_1.py
+++ juman_play_1.py
@@ -2,4 +2,14 @@ from pyknp import Jumanpp
 
 jumanapp = Jumanpp()
 j = jumanapp.analysis("ケーキを食べる")
+
+print("\n------------------------")
+print("Print list of objects:")
+print("------------------------")
 print(j.mrph_list())
+
+print("\n------------------------------------------------------")
+print("Print morpheme midashi from each object in the list:")
+print("------------------------------------------------------")
+print([morpheme.midasi for morpheme in j.mrph_list()])
+
